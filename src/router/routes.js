47b4d25e95557ded56f98a96fd8c8e150610545c
李聪 <lc182540@163.com>
2021-01-08 10:55:23 +0800

    feat: 首页

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,16 @@
 import Test from '@/views/test'
 import NotFound from '@/views/NotFound'
 
+function requireView(url) {
+  return () => import(`@/views/${url}`)
+}
+
 export default [
+  {
+    path: '/',
+    name: 'index',
+    component: requireView('index')
+  },
   {
     path: '/test',
     name: 'test',

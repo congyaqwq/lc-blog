9e1e1dd472a718af20c8187d97bb211749a166c5
李聪 <lc182540@163.com>
2021-01-11 18:42:31 +0800

    feat:切换页面动画

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <base-header></base-header>
-  <router-view class="main-content"></router-view>
-  <base-footer></base-footer>
+  <transition name="fade">
+    <router-view class="main-content"></router-view>
+  </transition>
+  <!-- <base-footer></base-footer> -->
 </template>
 
 <script>
 import BaseHeader from "./layouts/BaseHeader";
-import BaseFooter from "./layouts/BaseFooter";
+// import BaseFooter from "./layouts/BaseFooter";
 
 export default {
   name: "App",
   components: {
-    BaseHeader,
-    BaseFooter
+    BaseHeader
+    // BaseFooter
+  },
+  data() {
+    return {};
   }
 };
 </script>
@@ -25,7 +30,19 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   .main-content {
+    position: relative;
     padding: 30px 15%;
   }
+}
+
+// 切换动画
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
